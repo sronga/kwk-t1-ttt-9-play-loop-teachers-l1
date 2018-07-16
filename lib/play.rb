ce9db0709725c21821f042a5993c8d5f6e281e4f
sronga <sr899@georgetown.edu>
2018-07-16 22:36:11 +0000

    automatically backed up by learn

--- lib/play.rb
+++ lib/play.rb
@@ -37,7 +37,7 @@ end
 
 # Define your play method below
 def play(board)
-  while board != ["X","X","X","X","X","X","X","X","X",]
+  while [board] != ["X","X","X","X","X","X","X","X","X",]
   turn(board)
 end
 end
